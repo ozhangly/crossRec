f47d766df5788c3c4a4d8206c136407a1a9de9fd
MaxBlack <963137330@qq.com>
2024-04-15 17:41:26 +0800

    parser

--- utility/parser.py
+++ utility/parser.py
@@ -4,12 +4,26 @@ import argparse
 def arg_parse():
     parser = argparse.ArgumentParser(description='crossRec run config')
 
-    parser.add_argument('--dict_path', nargs='?', default='metadata/dict/', help='the dict file path')
-    parser.add_argument('--graph_path', nargs='?', default='metadata/graph/', help='the graph file path')
-    parser.add_argument('--ground_truth_path', nargs='?', default='metadata/groundtruth/')
-    parser.add_argument('--similarities_path', nargs='?', default='metadata/similarity/', type=str)
+    parser.add_argument('--dataset', nargs='?', default='CF_4_5')
+
+    parser.add_argument('--config_path', nargs='?', default='metadata/config')
+
+    parser.add_argument('--dict_path', nargs='?', default='dict')
+
+    parser.add_argument('--graph_path', nargs='?', default='graph')
+
+    parser.add_argument('--output_path', nargs='?', default='output')
+
+    parser.add_argument('--ground_truth_path', nargs='?', default='ground_truth')
+
+    parser.add_argument('--similarities_path', nargs='?', default='similarity')
+
+    parser.add_argument('--recommendation_path', nargs='?', default='recommendation')
+
     parser.add_argument('--numofneighbours', nargs='?', default=10, type=int)
-    parser.add_argument('--recommendation_path', nargs='?', default='metadata/recommendation/')
+
     parser.add_argument('--recommend_top_n', nargs='?', type=int, default=10)
+
+    parser.add_argument('--remove_n', nargs='?', type=int, default=5)
 
     return parser.parse_args()
