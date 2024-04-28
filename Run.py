d97bdc97edf8879d4dc63fc94bd151e72c53d43f
MaxBlack <963137330@qq.com>
2024-04-28 15:18:15 +0800

    Run process

--- Run.py
+++ Run.py
@@ -1,15 +1,27 @@
+"""
+二、运行程序
+"""
+import os
+import time
+
 from RecommendationEngine import *
 from MultiSimilarityComputation import *
 from Validation import save_recommend_and_result
 from utility.GenerateData import create_data
 
-
-# 这个代码改起来还是费点事, 得大改动，而且这个实验项目后期会非常大
 training_dataset = './training dataset/'
 args = arg_parse()
 if __name__ == '__main__':
     # 创建数据, 加上进度条
+    start_time = time.time()
     create_data(training_dataset + args.dataset)
-    cos_similarity(training_dataset + args.dataset)         # 这步耗时是大哥
-    Recommendation().user_based_recommendation(training_dataset + args.dataset)
+    cos_similarity(training_dataset + args.dataset)         # 调用的多线程计算相似度
+    Recommendation().user_based_recommendation(training_dataset + args.dataset)     # 这里可能会出现除零错误
     save_recommend_and_result(args.output_path, args.dataset, training_dataset)
+
+    # similarity 这个文件目录占用存储太大了，计算完就删除
+    print('正在删除使用完的文件')
+    os.system('rmdir /Q /S .\\training dataset\\' + args.dataset + '\\dict')
+    os.system('rmdir /Q /S .\\training dataset\\' + args.dataset + '\\graph')
+    os.system('rmdir /Q /S .\\training dataset\\' + args.dataset + '\\similarity')
+    print('删除完成')
